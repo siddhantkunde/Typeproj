d76a8489753fd9e0e44e7ef27a4b0718d588ba7d
siddhant <siddhantkunde@gmail.com>
2022-06-02 13:14:15 +0530

    error in product_route

--- src/routes/ProductRouter.ts
+++ src/routes/ProductRouter.ts
@@ -2,6 +2,7 @@ import Products from '../models/products';
 const router = require('express').Router();
 const verify=require('./verify_route')
 import express ,{Application,NextFunction,Request,Response}from 'express';
+import { error } from 'console';
 
 //fetch all items
 router.get('/product', async(req:Request,res:Response,next:NextFunction) => {
@@ -14,19 +15,7 @@ router.get('/product', async(req:Request,res:Response,next:NextFunction) => {
 })
 
 //fetch an item
-router.post("/create/product", (req:Request,res:Response,next:NextFunction) => {
-    return Products.query().insert({
-     Uid: req.body.Uid,
-     name:req.body.name,
-     category:String(req.body.category),
-     price:req.body.price
-   })
-   .returning(["id", "Uid", "name", "category","price"])
-   .then((products: any[]) => {
-      res.json(products[0])
-   })
-   .catch((error: any) => res.status(400).send(error))
-})
+
 // router.get('/product/:id', async(req:Request,res:Response,next:NextFunction) => {
 //     try{
 //         const item = await Products.findOne({_id: req.params.id})
@@ -40,21 +29,72 @@ router.post("/create/product", (req:Request,res:Response,next:NextFunction) => {
 // })
 
 //create an item
-router.post('/items',verify, async(req:Request,res:Response,next:NextFunction) => {
-    console.log(req.body.Uid,req.body.price)
-    try {
-        const newproduct = await Products.query().insert({
-            Uid:req.body.Uid,
-            name:req.body.name,
-            category:req.body.category,
-            price:parseInt(req.body.price),
-        })
-        res.status(201).send('item added')
-    } catch (error) {
-        console.log({error})
-        res.status(400).send({message: "error"})
-    }
-})
+router.post('/product',async(req:Request,res:Response,next:NextFunction) => {
+    const User_id = Number(req.body.Uid)
+    const Pname = String(req.body.name)
+    const Pcategory = String(req.body.category)
+    const Pprice = Number(req.body.price)
+  
+    const newproduct = new Products ({ User_id,Pname,Pcategory,Pprice })
+  
+    const upload = await Products.query().insert({
+      Uid: User_id,
+      name: Pname,
+      category: Pcategory,
+      price:Pprice,
+
+    })
+    res.send({message: "Item Uploaded!"})
+  });
+
+//     // validate request
+//     if(!req.body.Uid ||!req.body.name || !req.body.category || !req.body.price) {
+//         return res.status(400).send({
+//             success: false,
+//             message: "Please enter product name and price"
+//         });
+//     }
+
+//     // create a product
+//     const newproduct = new Products(
+//         {
+//             Uid:req.body.Uid,
+//             name: req.body.name,
+//             price: req.body.price
+//         }
+//     );
+
+//     // save product in the database.
+//     Products.save()
+//         .then((data: any) => {
+//             res.send({
+//                 success: true,
+//                 message: 'Product successfully created',
+//                 data: data
+//             });
+//         }).catch((err: { message: any; }) => {
+//         res.status(500).send({
+//             success: false,
+//             message: err.message || "Some error occurred while creating the product."
+//         });
+//     });
+// });
+
+// router.post('/items',verify, async(req:Request,res:Response,next:NextFunction) => {
+//     console.log(req.body.Uid,req.body.price)
+//     try {
+//         const newproduct = await Products.query().insert({
+//             Uid:req.body.Uid,
+//             name:req.body.name,
+//             category:req.body.category,
+//             price:parseInt(req.body.price),
+//         })
+//         res.status(201).send('item added')
+//     } catch (error) {
+//         console.log({error})
+//         res.status(400).send({message: "error"})
+//     }
+// })
 
 //update an item
 router.put('/product/:id', verify, async(req:Request,res:Response,next:NextFunction) => {
@@ -97,3 +137,6 @@ router.delete('/product/:id', verify, async(req:Request,res:Response,next:NextFu
 
 
 module.exports = router
+
+
+
